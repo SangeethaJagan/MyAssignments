4673097451ffba04d142bc62e78ba224cc964bfe
SangeethaJagan <142719649+SangeethaJagan@users.noreply.github.com>
2023-08-21 18:13:19 +0530

    Add files via upload

---
+++ Class1/PositiveorNegative.java
@@ -0,0 +1,20 @@
+package assignment.Week1.Class1;
+
+public class PositiveorNegative {
+
+	public void type()
+	{
+		int i=-10;
+		if(i>=0)
+		System.out.println(i+" is a Positive Number");
+		else
+		System.out.println(i+" is a Negative Numbner");
+		
+	}
+	
+	public static void main(String[] args) {
+	  PositiveorNegative P=new PositiveorNegative();
+	  P.type();
+	  
+	}
+}
